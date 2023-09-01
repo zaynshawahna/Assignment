39005cbdcb1c19ea78efdc57112d3d421b4967a6
zaynshawahna <136936745+zaynshawahna@users.noreply.github.com>
2023-09-02 02:25:30 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './ract1/Header';
+import Main from './ract1/Main';
+import Button from './ract1/Button';
+import Sidebuttons from './ract1/Sidebuttons';
+
+
+function App() {
+
+  return  <div >
+ <Header id="s1" name="Gaza Sky Geeks " href="https://gazaskygeeks.com/" target="_blank"></Header>
+ 
+ <Main title="Post Component" p="Design a Post component to display the main content of the webpage. This component should include the title of the post and body of the post. Position this component under the Page Header on the left-hand side.">
+      
+      </Main>
+     
+      <Main id="s3" title="Assignment #1" p="Reusability: Ensure that the Page Header, Post, and Button components are designed to be reusable. They should be flexible and easily adaptable for various parts of the webpage.
+  Component Structure: Organize your components in a modular way.">
+        
+        </Main>
+       <Sidebuttons>
+        
+       </Sidebuttons>
+
+
+        
+<Main id="s3" title="Assignment #1" p="Reusability: Ensure that the Page Header, Post, and Button components are designed to be reusable. They should be flexible and easily adaptable for various parts of the webpage.
+  Component Structure: Organize your components in a modular way.">
+        
+        </Main>
+            
+      <Main id="s3" title="Assignment #1" p="Reusability: Ensure that the Page Header, Post, and Button components are designed to be reusable. They should be flexible and easily adaptable for various parts of the webpage.
+  Component Structure: Organize your components in a modular way.">
+        
+        </Main>
+            
+      <Main id="s3" title="Assignment #1" p="Reusability: Ensure that the Page Header, Post, and Button components are designed to be reusable. They should be flexible and easily adaptable for various parts of the webpage.
+  Component Structure: Organize your components in a modular way.">
+        
+        </Main>
+        
+   </div>;
+}
+
+export default App;
---
+++ src/ract1/Sidebuttons.js
@@ -0,0 +1,21 @@
+
+import Button from "./Button";
+import "./Sidebuttons.css";
+export default function Sidebuttons(){
+    return <div className='Buttons'>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+        <Button type="button" id="button" data="Go"></Button>
+
+         <Button type="button" id="last" data="Go"></Button>
+    </div>
+ }
